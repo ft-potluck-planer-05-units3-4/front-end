93e24c9834f791ea7180930cc69bde2703633d04
bryan <bryan.guner@gmail.com>
2021-06-22 16:50:12 -0400

    private route w auth

--- src/utils/privateRoute.js
+++ src/utils/privateRoute.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { Route, Redirect } from "react-router-dom";
+// 
+// function PrivateRoute({ isLoggedIn, ...rest }) {
+//   return isLoggedIn ? <Route {...rest} /> : <Redirect to="/" />;
+// }
+// 
+// const state2props = (state) => ({
+//   isLoggedIn: state.isLoggedIn,
+// });
+// 
+// export default connect(state2props)(PrivateRoute);
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom';
 
-function PrivateRoute({ isLoggedIn, ...rest }) {
-  return isLoggedIn ? <Route {...rest} /> : <Redirect to="/" />;
+
+const Privateroute = ({component:Component, ...rest})=>{
+    return <Route{...rest} render ={(props)=>{
+        if(localStorage.getItem('token')){
+            return <Component {...props}/>
+        }
+        else{
+            return <Redirect to="/login"/>
+        }
+    }}/>
 }
 
-const state2props = (state) => ({
-  isLoggedIn: state.isLoggedIn,
-});
-
-export default connect(state2props)(PrivateRoute);
+export default Privateroute;
