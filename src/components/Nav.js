0385c09051b3e30e267462fb3728bb776695ceaa
Ethan Edmond <eedmond@uccs.edu>
2021-06-24 17:23:49 -0600

    Added reactstrap buttons instead of the nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-
+import { Button } from 'reactstrap';
 import { setEvents } from '../actions/eventActions';
 
 import Inviting from './Inviting';
@@ -21,35 +21,35 @@ function Nav(props) {
   useEffect(()=> {
     api.get('/events')
       .then(res => {
-	const returnEvents = res.data.map(event => {
-	  return {
-	    ...event,
-	    food: api.get(`/events/${event.id}/food`),
-	    guests: api.get(`/events/${event.id}/guests`)
-	  };
-	});
-	returnEvents.forEach(event => {
-	  event.food
-	    .then(res => {
-	      event.food = res.data;
-	    })
-	    .catch(alert);
-	  event.guests
-	    .then(res => {
-	      event.guests = res.data;
-	    })
-	    .catch(alert);
-	});
-	const allApiCalls = returnEvents
-	      .map(event => event.food)
-	      .concat(returnEvents
-		      .map(event => event.guests));
-	
-	Promise.all(allApiCalls)
-	  .then(vals => {
-	    dispatch(setEvents(returnEvents));
-	  })
-	  .catch(alert);
+  const returnEvents = res.data.map(event => {
+    return {
+      ...event,
+      food: api.get(`/events/${event.id}/food`),
+      guests: api.get(`/events/${event.id}/guests`)
+    };
+  });
+  returnEvents.forEach(event => {
+    event.food
+      .then(res => {
+        event.food = res.data;
+      })
+      .catch(alert);
+    event.guests
+      .then(res => {
+        event.guests = res.data;
+      })
+      .catch(alert);
+  });
+  const allApiCalls = returnEvents
+        .map(event => event.food)
+        .concat(returnEvents
+          .map(event => event.guests));
+
+  Promise.all(allApiCalls)
+    .then(vals => {
+      dispatch(setEvents(returnEvents));
+    })
+    .catch(alert);
       })
       .catch(alert);
   }, [dispatch]);
@@ -57,35 +57,41 @@ function Nav(props) {
   return (
     <>
       <header>
-	<Switch>
-	  <Route path='/attendee'>
-	    <Link to='/organizer'>
-	      Organizer Menu
-	    </Link>
-	  </Route>
-	  <Route path='/organizer'>
-	    <Link to='/attendee'>
-	      Attendee Menu
-	    </Link>
-	    <Link to='/add-event'>
-	      Add Potluck
-	    </Link>
-	  </Route>
-	</Switch>
+        <Switch>
+          <Route path='/attendee'>
+            <Link to='/organizer'>
+              <Button>
+                Organizer Menu
+              </Button>
+            </Link>
+          </Route>
+          <Route path='/organizer'>
+            <Link to='/attendee'>
+              <Button>
+                Attendee Menu
+              </Button>
+            </Link>
+            <Link to='/add-event'>
+              <Button>
+                Add Potluck
+              </Button>
+            </Link>
+          </Route>
+        </Switch>
       </header>
       <main>
-	<Switch>
-	  <Route path='/invite-to/:id' component={Inviting}/>
-	  <Route path='/attendee'>
-	    <InvitedList/>
-	  </Route>
-	  <Route path='/organizer'>
-	    <EventsList/>
-	  </Route>
-	  <Route path='/'>
-	    <Redirect to='/organizer'/> {/* Replace Me */}
-	  </Route>
-	</Switch>
+        <Switch>
+          <Route path='/invite-to/:id' component={Inviting}/>
+          <Route path='/attendee'>
+            <InvitedList/>
+          </Route>
+          <Route path='/organizer'>
+            <EventsList/>
+          </Route>
+          <Route path='/'>
+            <Redirect to='/organizer'/> {/* Replace Me */}
+          </Route>
+        </Switch>
       </main>
     </>
   )
