771f225a11efc4eb6b8b84f28dc6835e6d8a97e6
bryan <bryan.guner@gmail.com>
2021-06-24 05:16:07 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+// 
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import {useReducer} from './reducers/userReducer'
+import { Provider } from 'react-redux';
+
+
+const store = createStore(useReducer);
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,79 @@
-const initialState = {};
+// const initialState = {};
+// 
+// const userReducer = (state = initialState, action) => {
+//   switch(action.type) {
+//   default:
+//     return state;
+//   }
+// };
+// 
+// export default userReducer;
+import {
+  REGISTER_LOADING,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  LOGIN_LOADING,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from '../actions/userAction';
 
-const userReducer = (state = initialState, action) => {
-  switch(action.type) {
-  default:
-    return state;
-  }
+const initialState = {
+  username: '',
+  password: '',
+  email: '',
+  error: '',
+  isLoading: false,
+  user_id: '',
+  token: window.localStorage.getItem( 'token' ) || '',
+  message: '',
+  isLoggedIn: false,
 };
 
-export default userReducer;
+export const userReducer = ( state = initialState, action ) => {
+  switch ( action.type ) {
+    case REGISTER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        username: action.payload.username,
+          password: action.payload.password,
+          email: action.payload.email,
+          isLoading: false,
+      };
+    case REGISTER_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+          error: action.payload,
+      };
+    case LOGIN_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+          error: '',
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+          username: action.payload.username,
+          user_id: action.payload.user_id,
+          token: action.payload.token,
+          message: action.payload.message,
+          isLoggedIn: true,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+          error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
