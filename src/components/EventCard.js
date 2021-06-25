06177f11cb02e31e7e0923f43b94c3b2c976789d
Ethan Edmond <eedmond@uccs.edu>
2021-06-24 18:08:12 -0600

    Used some reactstrap components

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Card, CardBody, CardTitle, CardHeader } from 'reactstrap';
 import { connect } from 'react-redux';
 
 import { deleteEvent, editEvent } from '../actions/eventActions';
@@ -67,61 +67,63 @@ function EventCard(props){
   };
 
   return (
-    <div className='event-card'>
-      <h3>{event.title}</h3>
-      <p>Location: {event.location}</p>
-      <p>Date: {event.month} {event.day}, {event.year}</p>
-      <p>Times: {event.start_time}-{event.end_time}</p>
-      {event.guests && (
-        <div className='guest-list'>
-          <h5>Who's Invited</h5>
-          <ul>
-            {event.guests.map(guest => <li key={guest.id}>{guest.name}</li>)}
-          </ul>
-        </div>
-      )}
-      { event.food && (
-        <div className='food-list'>
-          <h5>Food Requests</h5>
-          <ul>
-            { event.food.map(item => {
-              return (
-                <li key={item.id}>
-                  {item.name}, {item.quantity}
-                  <button onClick={onDelFoodMaker(item.id)}>&times;</button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-      <AddFoodForm onAddFood={onAddFood}/>
-      { deleteOpen ? (
-        <div className='delete-modal'>
-          Are you Sure?
-          <button onClick={onDelete}>
-            Yup
-          </button>
-          <button onClick={onDeleteClose}>
-            No I changed my mind
-          </button>
-        </div>
-      ) : (
-        <button onClick={onDeleteOpen}>
-          Delete Event
-        </button>
-      ) }
-      <Link to={`/invite-to/${event.id}`}>
-        <Button>
-          Invite More
-        </Button>
-      </Link>
-      <Link to={`/edit-event/${event.id}`}>
-        <Button>
-          Edit
-        </Button>
-      </Link>
-    </div>
+      <Card>
+        <CardHeader tag='h3'>{event.title}</CardHeader>
+        <CardBody>
+          <p>Location: {event.location}</p>
+          <p>Date: {event.month} {event.day}, {event.year}</p>
+          <p>Times: {event.start_time}-{event.end_time}</p>
+          {event.guests && (
+            <div className='guest-list'>
+              <h5>Who's Invited</h5>
+              <ul>
+                {event.guests.map(guest => <li key={guest.id}>{guest.name}</li>)}
+              </ul>
+            </div>
+          )}
+          { event.food && (
+            <div className='food-list'>
+              <h5>Food Requests</h5>
+              <ul>
+                { event.food.map(item => {
+                  return (
+                    <li key={item.id}>
+                      {item.name}, {item.quantity}
+                      <button onClick={onDelFoodMaker(item.id)}>&times;</button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+          <AddFoodForm onAddFood={onAddFood}/>
+          { deleteOpen ? (
+            <div className='delete-modal'>
+              Are you Sure?
+              <button onClick={onDelete}>
+                Yup
+              </button>
+              <button onClick={onDeleteClose}>
+                No I changed my mind
+              </button>
+            </div>
+          ) : (
+            <button onClick={onDeleteOpen}>
+              Delete Event
+            </button>
+          ) }
+          <Link to={`/invite-to/${event.id}`}>
+            <Button>
+              Invite More
+            </Button>
+          </Link>
+          <Link to={`/edit-event/${event.id}`}>
+            <Button>
+              Edit
+            </Button>
+          </Link>
+        </CardBody>
+      </Card>
   );
 }
 
