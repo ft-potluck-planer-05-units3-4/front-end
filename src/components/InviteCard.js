304c9dfe310f9ccdbff7c09f53e370366b0a6ad2
Ethan Edmond <eedmond@uccs.edu>
2021-06-29 13:16:27 -0600

    Changed some of the tokens and did some styling to InviteCard

--- src/components/InviteCard.js
+++ src/components/InviteCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { Card, CardHeader, CardBody, CardSubtitle, CardText } from 'reactstrap';
 
 import InviteFoodCard from './InviteFoodCard';
 
@@ -9,7 +10,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://potluck-planner1.herokuapp.com/api',
   headers: {
-    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJuYW1lIjoidGVzdGVyIiwidXNlcm5hbWUiOiJ0ZXN0ZXIiLCJpYXQiOjE2MjQzNjUwNTQsImV4cCI6MTYyNDk2OTg1NH0.pdyE9DfHyUiz1N8hZQI7veq1c-hRad1hg4kcSFVKg6c'
+    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJuYW1lIjoiVGVzdGVyIiwidXNlcm5hbWUiOiJUZXN0ZXIiLCJpYXQiOjE2MjQ5OTMxMDEsImV4cCI6MTYyNTU5NzkwMX0.-c7yrBn4ZTE7VQECaOe7Ke9a0FIDJynxGQS3185VRlE'
   }
 });
 
@@ -25,22 +26,27 @@ function InviteCard({invite}) {
       .catch(alert);
   }
   return (
-    <div className='invite-card'>
-      <h3>{invite.title}</h3>
-      <p>Location: {invite.location}</p>
-      <p>Date: {invite.month} {invite.day}, {invite.year}</p>
-      <p>Times: {invite.start_time}-{invite.end_time}</p>
-      { !!invite.food.length && (
-        <>
-          <h5>Food Requests: </h5>
-          <ul>
-            {invite.food.map(item => <InviteFoodCard key={item.id} item={item} invite={invite}/>)}
-          </ul>
-        </>
-      )}
-      {/* <button>RSVP</button> */}
-      <button onClick={onDecline}>Decline Invitation</button>
-    </div>
+    <Card>
+      <CardHeader tag='h3'>{invite.title}</CardHeader>
+      <CardBody>
+        <CardSubtitle className='text-muted'>Location:</CardSubtitle>
+        <CardText>{invite.location}</CardText>
+        <CardSubtitle className='text-muted'>Date:</CardSubtitle>
+        <CardText>{invite.month} {invite.day}, {invite.year}</CardText>
+        <CardSubtitle className='text-muted'>Times:</CardSubtitle>
+        <CardText>{invite.start_time}-{invite.end_time}</CardText>
+        { !!invite.food.length && (
+          <>
+            <h5>Food Requests: </h5>
+            <ul>
+              {invite.food.map(item => <InviteFoodCard key={item.id} item={item} invite={invite}/>)}
+            </ul>
+          </>
+        )}
+        {/* <button>RSVP</button> */}
+        <button onClick={onDecline}>Decline Invitation</button>
+      </CardBody>
+    </Card>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://potluck-planner1.herokuapp.com/api',
   headers: {
-    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJuYW1lIjoidGVzdGVyIiwidXNlcm5hbWUiOiJ0ZXN0ZXIiLCJpYXQiOjE2MjQzNjUwNTQsImV4cCI6MTYyNDk2OTg1NH0.pdyE9DfHyUiz1N8hZQI7veq1c-hRad1hg4kcSFVKg6c'
+    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJuYW1lIjoiVGVzdGVyIiwidXNlcm5hbWUiOiJUZXN0ZXIiLCJpYXQiOjE2MjQ5OTMxMDEsImV4cCI6MTYyNTU5NzkwMX0.-c7yrBn4ZTE7VQECaOe7Ke9a0FIDJynxGQS3185VRlE'
   }
 });
 
